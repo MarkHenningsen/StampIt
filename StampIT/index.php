42b5e0c146061db030f323a7b106eb5c34859b16
Mark Henningsen <32099453+MarkHenningsen@users.noreply.github.com>
2019-01-12 15:24:11 +0100

    Add files via upload

---
+++ StampIT/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>StampIt</title>
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/navbar.css">
+
+
+</head>
+<body>
+
+
+<nav class="navbar navbar">
+ </nav>
+
+<div class="menu-tab">
+
+    <div id="one"></div>
+    <div id="two"></div>
+    <div id="three"></div>
+</div>
+<div class="menu-hide">
+  <nav>
+   <div class="profile-section">
+      <div class="profile-section-inner">
+         <h4>profil sektion</h3>
+      </div>
+      
+      </div>
+    <ul>
+      <li><i class="fas fa-home"></i><a href="#">Hjem</a></li>
+      <li><i class="fas fa-cog"></i><a href="#">Instillinger</a></li>
+      <li><i class="fas fa-sign-out-alt"></i><a href="#">Log ud</a></li>
+
+    </ul>
+  </nav>
+</div>
+
+
+
+
+<!--
+   <footer class="footer">
+      <div class="container">
+         <div class="row">
+         <div class="col-md-3">HJEM</div>
+         <div class="col-md-3">REWARDS</div>
+         <div class="col-md-3">MAP</div>
+         <div class="col-md-3">PROFILE</div>
+      </div>
+   </div>
+</footer>
+
+!-->
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
+<script src="js/navbar.js"></script>
+
+</body>
+</html>
